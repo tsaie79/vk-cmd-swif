372e993c82efa55dead1edbcb3e093bffb738441
tsai <tsaie79@gmail.com>
2023-10-04 13:57:20 -0400

    test run

--- test_wf/cmds_swif.sh
+++ test_wf/cmds_swif.sh
@@ -3,5 +3,14 @@
 # show wfs 
 alias lswf="swif2 list"
 
+# grep job_id from wf
+function statwf { while true; do swif2 status $1 | grep dispatched && squeue -u tsai && sleep $2; done }
+
 # delete wf
 function delwf { swif2 cancel $1 -delete; }
+
+#create wf
+function genwf { swif2 import -file $1; }
+
+#run wf 
+function runwf { swif2 run $1; }
